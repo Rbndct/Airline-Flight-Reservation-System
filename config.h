0f7992e085cbf86d0226c0b23bef739844209a1c
Rbndct <rbenedict.maagma@gmail.com>
2025-03-25 20:58:20 +0800

    Add configuration header for flight reservation system constants

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ROWS 5                     // Number of rows in each flight's seating arrangement
+#define COLS 6                     // Number of columns in each flight's seating arrangement
+#define TOTAL_SEATS (ROWS * COLS)  // Total number of seats per flight
+#define FLIGHTS 3                  // Total number of flights
+
+#endif
