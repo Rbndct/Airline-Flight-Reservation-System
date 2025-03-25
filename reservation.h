5198069a910fefb70a05f18b9d9076aa017b388f
Rbndct <rbenedict.maagma@gmail.com>
2025-03-25 21:31:18 +0800

    Remove unused getSeatSelection function from reservation.h

--- reservation.h
+++ reservation.h
@@ -5,7 +5,6 @@ void initializeSeats(int flightSeats[], int size);
 int countAvailableSeats(int seats[], int totalSeats);
 void reserveTicket(int flightSeats101[], int flightSeats202[], int flightSeats303[], int prices[]);
 int getFlightNumber();
-int getSeatSelection(int *row, int *col);
 int isSeatAvailable(int flightNumber, int row, int col, int flightSeats101[], int flightSeats202[],
                     int flightSeats303[]);
 void processSeatReservation(int flightNumber, int row, int col, int flightSeats101[],
